25a3b6b89c42a0a62fb48924da45733ee2d9926d
Marcelo Valois <mmv147.valois@gmail.com>
2022-04-21 17:10:24 -0300

    metodos property e de tipos criados

--- src/cinCheck.js
+++ src/cinCheck.js
@@ -1,12 +1,53 @@
 class CinCheck {
     constructor() {}
 
+    static integer() {
+        let min = -10000;
+        let max = 10000;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    static float() {
+        let min = -10000;
+        let max = 10000;
+        return Math.random() * (max - min + 1) + min;
+    }
+
+    static nat() {
+        let min = 0;
+        let max = 10000;
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    static boolean() {
+        return Math.random() > 0.5 ? true : false;
+    }
+
+    static string() {
+        let randomString = '';
+        let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let size = Math.floor(Math.random() * (25 - 10 + 1)) + 10;
+        for (let i = 0; i < size; i++) {
+            randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return randomString;
+    }
+
+    static array(type) {
+        const arraySize = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+        const array = []
+        for (let i = 0; i < arraySize; i++) {
+            array.push(type());
+        }
+        return array;
+    }
+
     static property (num, callback) {
         callback(num);
     }
 }
 
 const random = Math.random();
-CinCheck.property(random, data => console.log(data))
 
-// string, integer, nat, boolean, number
+for (let i=0; i<1; i++)
+    CinCheck.property(CinCheck.array(CinCheck.string), data => console.log(data));
